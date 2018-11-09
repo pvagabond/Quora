f0709d20a59e6e6c4dd7e07b9542bf537317c7a0
Yang Su <pvagabond@gmail.com>
2018-11-08 23:33:04 -0800

    add random forest classifier

--- solver/quora.py
+++ solver/quora.py
@@ -10,6 +10,7 @@ import pandas as pd  # data processing, CSV file I/O (e.g. pd.read_csv)
 
 from nltk.corpus import stopwords
 from nltk.stem import PorterStemmer
+from nltk import WordNetLemmatizer
 from sklearn.feature_extraction.text import CountVectorizer
 import os
 import re
@@ -27,21 +28,25 @@ def clean_up(text):
     # tokenize text
     text = tokenize(text)
     # remove stopwords
-    ps = PorterStemmer()
     stop_words = set(stopwords.words('english'))
-    return [ps.stem(word) for word in text if word not in stop_words]
+
+    # ps = PorterStemmer()
+    # return [ps.stem(word) for word in text if word not in stop_words]
+
+    # apply Lemmatizer
+    wn = WordNetLemmatizer()
+    return [wn.lemmatize(word) for word in text if word not in stop_words]
+
 
 
 print(os.listdir("../input"))
 
 # Any results you write to the current directory are saved as output.
 
-train = pd.read_csv("../input/train.csv")
+train = pd.read_csv("../input/sample.csv")
 
 train.columns = ['id', 'question', 'target']
 
-# truncate data
-train = train[0:10]
 
 train['tokenizer'] = train['question'].apply(tokenize)
 
@@ -53,3 +58,27 @@ print(count_vector.get_feature_names())
 
 # add new features
 train['len'] = train['question'].apply(lambda x: len(x) - x.count(" "))
+
+
+# plot graphs
+def plot_histogram():
+    from matplotlib import pyplot
+
+    bins = np.linspace(0, 20, 21)
+    pyplot.hist(train[train['target'] == 1]['len'], bins, alpha=0.5, density=0.5, label="insincere")
+    pyplot.hist(train[train['target'] == 0]['len'], bins, alpha=0.5, density=0.5, label="sincere")
+    pyplot.legend(loc="upper right")
+    pyplot.title("histogram for question length")
+    pyplot.show()
+
+
+X_features = pd.concat([train['len'], pd.DataFrame(X_counts.toarray())], axis=1)
+
+from sklearn.ensemble import RandomForestClassifier
+from sklearn.model_selection import KFold, cross_val_score
+
+
+rfc = RandomForestClassifier(n_jobs=-1)
+k_fold = KFold(n_splits=5)
+cross_val_score(rfc, X_features, train['id'], cv=k_fold, scoring="accuracy", n_jobs=-1)
+
